8d3ebe3efced4e7d88fc7b7e9a306ee92d9f0d9f
fred21O4 <67189813+fred21O4@users.noreply.github.com>
2024-09-24 15:04:42 +1200

    setup dev environment

--- flake.nix
+++ flake.nix
@@ -4,42 +4,54 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
-        devShells.default = mkShell rec {
-          buildInputs = [
-            # Rust
-            rust-bin.stable.latest.default
-            trunk
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [rust-overlay.overlays.default];
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          # Rust
+          rust-analyzer
+          rust-bin.stable.latest.default
+          trunk
 
-            # misc. libraries
-            openssl
-            pkgconfig
+          # misc. libraries
+          openssl
+          pkg-config
 
-            # GUI libs
-            libxkbcommon
-            libGL
-            fontconfig
+          # GUI libs
+          libxkbcommon
+          libGL
+          fontconfig
 
-            # wayland libraries
-            wayland
+          # wayland libraries
+          wayland
 
-            # x11 libraries
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libX11
+          # x11 libraries
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libX11
+        ];
 
-          ];
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+      };
+    });
 
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-        };
-      });
+    formatter = forEachSupportedSystem ({pkgs}: pkgs.alejandra);
+  };
 }
